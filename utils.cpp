2ba13ec306cc6c3c651eb878f7f0a90fd2b83057
hdkhang6803 <hdkhang6803@gmail.com>
2023-12-28 00:47:19 +0700

    bai 1 done, do not know other optimization better than n*k

---
+++ utils.cpp
@@ -0,0 +1,11 @@
+#include "utils.h"
+
+//the time complexity: O(log(a + b)).
+int gcd(int a, int b) {
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
