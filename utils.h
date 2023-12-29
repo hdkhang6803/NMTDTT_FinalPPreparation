4e22d1c6d9f0de3ef42e56dc9b50894f71934055
hdkhang6803 <hdkhang6803@gmail.com>
2023-12-30 01:00:13 +0700

    add binary search, lower bound, upper bound

--- utils.h
+++ utils.h
@@ -1,4 +1,11 @@
 #pragma once
 
+#include <vector>
+
+using namespace std;
+
 #define ULLONG unsigned long long int 
 int gcd(int a, int b);
+int upper(vector<int> a, int x, int lo, int hi);
+int lower(vector<int> a, int x, int lo, int hi);
+int BinarySearch(vector<int> a, int x);
