42cd6f35b008fb41bf65e8070ca2970e52dd448b
Hoang Do <hoang2003ngolike@gmail.com>
2024-01-03 12:53:41 +0700

    Bai_22 update new method

--- Bai_22.cpp
+++ Bai_22.cpp
@@ -51,6 +51,54 @@ void dfsCount(int node, int parent, int& count, vector<bool>& visited, vector<ve
     }
 }
 
+void dfsMark(int node, int parent, int& number, vector<bool>& visited, vector<int>& nodeNumbers, vector<vector<int>>& graph, vector<pair<int, int>>& bridges) {
+    visited[node] = true;
+    nodeNumbers[node] = number;
+    for (int neighbor : graph[node]) {
+        if (neighbor == parent) {
+            continue;
+        }
+        if (visited[neighbor]) {
+            continue;
+        }
+        bool isBridge = false;
+        for (const auto& bridge : bridges) {
+            if ((bridge.first == node && bridge.second == neighbor) || (bridge.first == neighbor && bridge.second == node)) {
+                isBridge = true;
+                break;
+            }
+        }
+        if (isBridge) {
+            number++;
+        }
+        dfsMark(neighbor, node, number, visited, nodeNumbers, graph, bridges);
+    }
+}
+
+void markNodesWithNumbers(vector<vector<int>>& graph, vector<pair<int, int>>& bridges, vector<int>& nodeNumbers) {
+    int N = graph.size() - 1;
+    vector<bool> visited(N + 1, false);
+    int number = 1;
+
+    for (int i = 1; i <= N; i++) {
+        if (!visited[i]) {
+            dfsMark(i, -1, number, visited, nodeNumbers, graph, bridges);
+        }
+    }
+}
+
+
+void dfsSubtree(vector<vector<int>>& adj, vector<int>& sub, int u, int p) {
+    sub[u] = 1;
+    for (int v : adj[u]) {
+        if (v == p) continue;
+        dfsSubtree(adj, sub, v, u);
+        sub[u] += sub[v];
+    }
+    // sub[u] * (adj.size() - sub[u]) is total weight of edge p - u added to ans
+    // ans += 1LL * sub[u] * (adj.size() - sub[u]);
+}
+
 int main() {
     int N, M;
     cin >> N >> M;
@@ -68,37 +116,65 @@ int main() {
 
     vector<pair<int, int>> bridges;
     findBridges(graph, bridges);
+    vector<bool> visited(N + 1, false);
+    int number = 1;
 
-    vector<int> result(M);
+    vector<int> nodeNumber(N);
 
-    for (int i = 0; i < M; i++) {
-        int u = edges[i].first;
-        int v = edges[i].second;
+    dfsMark(1, -1, number, visited, nodeNumber, graph, bridges);
 
-        bool isBridge = false;
-        for (const auto& bridge : bridges) {
-            if ((bridge.first == u && bridge.second == v) || (bridge.first == v && bridge.second == u)) {
-                isBridge = true;
-                break;
-            }
-        }
+    // for (int i = 0; i < M; i++) {
+    //     int u = edges[i].first;
+    //     int v = edges[i].second;
 
-        if (isBridge) {
-            int x = 0;
-            vector<bool> visited(N + 1, false);
+    //     bool isBridge = false;
+    //     for (const auto& bridge : bridges) {
+    //         if ((bridge.first == u && bridge.second == v) || (bridge.first == v && bridge.second == u)) {
+    //             isBridge = true;
+    //             break;
+    //         }
+    //     }
 
-            dfsCount(u, v, x, visited, graph);
-            int n = N - x;
-            result[i] = x * n;
-        } else {
-            result[i] = 0;
-        }
-    }
+    //     if (isBridge) {
+    //         int x = 0;
+    //         vector<bool> visited(N + 1, false);
+
+    //         dfsCount(u, v, x, visited, graph);
+    //         int n = N - x;
+    //         result[i] = x * n;
+    //     } else {
+    //         result[i] = 0;
+    //     }
+    // }
 
     // Print the results
-    for (int i = 0; i < M; i++) {
-        cout << result[i] << endl;
+    // for (int i = 1; i <= N; i++) {
+    //     cout << i << ' ' << nodeNumber[i] << endl;
+    // }
+
+    vector<vector<int>> graph2(N + 1);
+    for (int i = 1; i <= M; i++) {
+        // int u = edges[i].first;
+        // int v = edges[i].second;
+
+        int u = nodeNumber[bridges[i].first];
+        int v = nodeNumber[bridges[i].second];
+
+        graph2[u].push_back(v);
+        graph2[v].push_back(u);
     }
+
+    vector<int> sub(N + 1);
+    dfsSubtree(graph2, sub, 1, -1);
+
+    for (int i = 1; i <= M; i++) {
+        cout << i << "\n";
+        int u = nodeNumber[bridges[i].first];
+        int v = nodeNumber[bridges[i].second];
+
+        cout << u << ' ' << v << ' ' << sub[u] * (N - sub[u]) << endl;
+    }
+
 
     return 0;
 }
